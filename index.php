5e51981e8c779df91114403e63ad5d593f51ea99
oleksa <lelik4w@gmail.com>
2022-06-26 16:51:27 +0200

    update plugin version

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * Plugin URI: http://webtemplatemasters.com
  * Description: Integration of PayPal Express Checkout
  * Author: ThemeMakers
- * Version: 1.1.8
+ * Version: 1.1.9
  * Author URI: http://themeforest.net/user/ThemeMakers
  * Text Domain: tmm_paypal_checkout
  */
